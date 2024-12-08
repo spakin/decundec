2de1ea78afc90fd817a1c0cda95e073583018e95
Scott Pakin <scott+gpg@pakin.org>
2024-12-08 12:29:04 -0700

    Write function comments for test functions

--- decslices_test.go
+++ decslices_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spakin/decslices"
 )
 
+// TestSortIntsIdentity uses Sort to sort a list of ints in increasing
+// order.  The sort-key function is the identity.
 func TestSortIntsIdentity(t *testing.T) {
 	array := []int{-2407, -3170, -4255, 2026, 1976, 0, 4596, 7961, -5923, -3758, -7435, -8423, 9733, 4428, 2829, 2977, -2656, -7921, -2350, -1997, -1505, -4593, -7147, 5994, 2184, 6618}
 	expected := []int{-8423, -7921, -7435, -7147, -5923, -4593, -4255, -3758, -3170, -2656, -2407, -2350, -1997, -1505, 0, 1976, 2026, 2184, 2829, 2977, 4428, 4596, 5994, 6618, 7961, 9733}
@@ -18,6 +20,8 @@ func TestSortIntsIdentity(t *testing.T) {
 	}
 }
 
+// TestSortIntsReversed uses Sort to sort a list of ints in reverse order.
+// The sort-key function maps x to -x to achieve this ordering.
 func TestSortIntsReversed(t *testing.T) {
 	array := []int{-2407, -3170, -4255, 2026, 1976, 0, 4596, 7961, -5923, -3758, -7435, -8423, 9733, 4428, 2829, 2977, -2656, -7921, -2350, -1997, -1505, -4593, -7147, 5994, 2184, 6618}
 	expected := []int{9733, 7961, 6618, 5994, 4596, 4428, 2977, 2829, 2184, 2026, 1976, 0, -1505, -1997, -2350, -2407, -2656, -3170, -3758, -4255, -4593, -5923, -7147, -7435, -7921, -8423}
@@ -29,6 +33,8 @@ func TestSortIntsReversed(t *testing.T) {
 	}
 }
 
+// TestSortFuncStrings uses SortFunc to sort a list of strings in
+// increasing order by length.
 func TestSortFuncStrings(t *testing.T) {
 	words := []string{
 		"interdenominational",
